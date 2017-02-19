36b4800bfa0b32104aece105c691c08d45bed1ef
Mykola Bilochub <nbelochub@gmail.com>
2017-02-19 22:31:13 +0200

    Fix linter error for unused variables in tests
    PR-URL: https://github.com/metarhia/sandboxed-fs/pull/3

--- test.js
+++ test.js
@@ -5,8 +5,10 @@ const fs = sandboxedFs.bind('./test');
 
 fs.readFile('file.ext', (err, data) => {
   if (err) return console.log('Cannot read file');
+  console.log(data.toString());
 });
 
 fs.readFile('../../file.ext', (err, data) => {
   if (err) return console.log('Cannot read file');
+  console.log(data.toString());
 });
