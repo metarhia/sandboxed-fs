7fef2ddaffc8f684c2b01868bd45100efab5917e
Simon Schick <simonsimcity@gmail.com>
2017-11-01 20:20:49 +0400

    Broke up lines (link is still on 82 ...)

--- sandboxed-fs.js
+++ sandboxed-fs.js
@@ -13,12 +13,14 @@ const errorMessage = 'path must be a string';
 function makePathSafe(path) {
   const safePath = pathModule.resolve('/', path);
 
-  // As Windows is the only non-UNIX like platform supported by node (https://github.com/nodejs/node/blob/master/BUILDING.md#supported-platforms-1)
+  // As Windows is the only non-UNIX like platform supported by node
+  // https://github.com/nodejs/node/blob/master/BUILDING.md#supported-platforms-1
   if (isWindows()) {
     if (isUncPath(safePath)) {
       return safePath.substring(pathModule.parse(safePath).root.length);
     } else {
-      // If the path is a non-unc path on windows, the root is fixed to 3 characters like 'C:\'
+      // If the path is a non-unc path on windows, the root is fixed to 3
+      // characters like 'C:\'
       return safePath.substring(3);
     }
   }
